8d908ddb5208dd7f501b55bdda54dd405fc09915
ImagePineDeveloper <developer@imagepine.com>
2026-06-09 12:28:52 +0500

    feat: Add Google Analytics integration using Google Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import Navbar from "@/components/Navbar";
 import PrivacyBanner from "@/components/PrivacyBanner";
 import Footer from "@/components/Footer";
@@ -26,6 +27,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-K17SPE0RN6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-K17SPE0RN6');
+          `}
+        </Script>
+      </head>
       <body
         className={`${inter.variable} font-sans antialiased`}
         style={{
